bc5130191a6d1119a41d9ef8682640aa73a62962
shixuejun-neusoft <shixuejun@neusoft.co.jp>
2022-11-28 17:09:49 +0900

    fix view extended id. fix interrupt receivable pin. modify code format.

--- examples/Extended_MaskFilter/Extended_MaskFilter.ino
+++ examples/Extended_MaskFilter/Extended_MaskFilter.ino
@@ -38,48 +38,77 @@ long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
 
-MCP_CAN CAN0(19);                          // Set CS to pin 19
+/* Serial Output String Buffer */
+char msgString[128];
+
+#define REMOTE_CAN_ID_FORMAT 0x40000000
+#define EXTENDED_CAN_ID_FORMAT 0x80000000
+/* Set INT to pin 20 */
+#define CAN0_INT 20
+/* Set CS to pin  19 */
+MCP_CAN CAN0(19);
 
 void setup()
 {
   Serial.begin(115200);
-  if(CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_16MHZ) == CAN_OK) Serial.print("MCP2515 Init Okay!!\r\n");
-  else Serial.print("MCP2515 Init Failed!!\r\n");
-  pinMode(2, INPUT);                       // Setting pin 2 for /INT input
+  /* Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled. */
+  if(CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_16MHZ) == CAN_OK) {
+    Serial.println("MCP2515 Initialized Successfully!");
+  } else {
+    Serial.println("Error Initializing MCP2515...");
+  }
+  /* Configuring pin for INT input */
+  pinMode(CAN0_INT, INPUT);
 
-  CAN0.init_Mask(0,1,0x00FFFF00);                // Init first mask...
-  CAN0.init_Filt(0,1,0x00FFEE00);                // Init first filter...
-  CAN0.init_Filt(1,1,0x00FFDD00);                // Init second filter...
+  /* Init first mask... */
+  CAN0.init_Mask(0,1,0x00FFFF00);
+  /* Init first filter... */
+  CAN0.init_Filt(0,1,0x00FFEE00);
+  /* Init second filter... */
+  CAN0.init_Filt(1,1,0x00FFDD00);
+
+  /* Init second mask... */
+  CAN0.init_Mask(1,1,0x00FFFF00);
+  /* Init third filter... */
+  CAN0.init_Filt(2,1,0x00FFBB00);
+  // Init fourth filter... */
+  CAN0.init_Filt(3,1,0x00FFAA00);
+  /*Init fifth filter... */
+  CAN0.init_Filt(4,1,0x00FF8800);
+  /* Init sixth filter... */
+  CAN0.init_Filt(5,1,0x00FF7700);
   
-  CAN0.init_Mask(1,1,0x00FFFF00);                // Init second mask... 
-  CAN0.init_Filt(2,1,0x00FFBB00);                // Init third filter...
-  CAN0.init_Filt(3,1,0x00FFAA00);                // Init fourth filter...
-  CAN0.init_Filt(4,1,0x00FF8800);                // Init fifth filter...
-  CAN0.init_Filt(5,1,0x00FF7700);                // Init sixth filter...
-  
-  Serial.println("MCP2515 Library Mask & Filter Example...");
-  CAN0.setMode(MCP_NORMAL);                // Change to normal mode to allow messages to be transmitted
+  Serial.println("MCP2515 Library Extended Mask & Filter Example...");
+  /* Change to normal mode to allow messages to be transmitted */
+  CAN0.setMode(MCP_NORMAL);
 }
 
 void loop()
 {
-    if(!digitalRead(2))                    // If pin 2 is low, read receive buffer
-    {
-      CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
-      Serial.print("ID: ");
-      Serial.print(rxId, HEX);
-      Serial.print(" Data: ");
-      for(int i = 0; i<len; i++)           // Print each byte of the data
-      {
-        if(rxBuf[i] < 0x10)                // If data byte is less than 0x10, add a leading zero
-        {
-          Serial.print("0");
-        }
-        Serial.print(rxBuf[i], HEX);
-        Serial.print(" ");
-      }
-      Serial.println();
+  /* If pin CAN0_INT is low, read receive buffer */
+  if(!digitalRead(CAN0_INT)) {
+    /* Read data: len = data length, buf = data byte(s) */
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);
+    /* Determine if ID is standard (11 bits) or extended (29 bits) */
+    if((rxId & EXTENDED_CAN_ID_FORMAT) == EXTENDED_CAN_ID_FORMAT) {
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    } else {
+      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
     }
+    Serial.print(msgString);
+
+    /* Determine if message is a remote request frame. */
+    if((rxId & REMOTE_CAN_ID_FORMAT) == REMOTE_CAN_ID_FORMAT) {
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++) {
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+      }
+    }
+    Serial.println();
+  }
 }
 
 /*********************************************************************************************************
